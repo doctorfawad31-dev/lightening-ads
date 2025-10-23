c35988ac85b0146a790508b5943363791186e9e7
Muhammad-Faizan-Alam <alifaizanch3101@gmail.com>
2025-10-23 18:47:11 +0500

    footer links updated

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -108,7 +108,7 @@ const Blog = () => {
         <Header />
         
         {/* Hero Section */}
-        <section className="relative md:pt-20 pt-12 md:pb-16 pb-8 overflow-hidden">
+        <section className="relative pt-24 md:pb-16 pb-8 overflow-hidden">
           <div className="container mx-auto px-6 md:py-20 py-10">
             <div className="max-w-4xl mx-auto text-center">
               <h1 className="text-4xl md:text-6xl font-bold mb-6">
